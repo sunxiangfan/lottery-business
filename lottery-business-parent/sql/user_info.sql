f354d4705f2e1e4a8fcf74347e0c0fc7997f54d5
sunxiangfan <13111110495@163.com>
2018-05-15 16:08:31 +0800

    sql

--- lottery-business-parent/sql/user_info.sql
+++ lottery-business-parent/sql/user_info.sql
@@ -18,3 +18,26 @@ create table bank_card (
   card_holder 	varchar(16) not null comment '持卡人'
   primary key (id)
 )comment '银行卡信息';
+
+create table lottery_num (
+  id 						    varchar(32) not null comment 'ID',
+  lottery_code			varchar(8) not null comment '彩种',
+  lottery_num 			varchar(32) not null comment '开奖号码',
+  lottery_periods varchar(16) not null comment '开奖期数',
+  time 	  timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, comment '开奖时间'
+  primary key (id)
+)comment '开奖信息';
+
+create table lottery_record (
+  id 						    varchar(32) not null comment '购彩记录ID',
+  user_id			    varchar(32) not null comment '用户ID',
+  lottery_code			varchar(8) not null comment '彩种',
+  purchase_num 			varchar(16) not null comment '购彩号码',
+  lottery_periods varchar(16) not null comment '购彩期数',
+  multiple integer (8) not null comment '倍数',
+  lottery_periods DECIMAL(11,2) not null comment '购彩金额',
+  status			varchar(8) not null comment '购彩状态',
+  prize_status			varchar(8) not null comment '中奖状态',
+  time 	  timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, comment '购买时间'
+  primary key (id)
+)comment '购彩记录';
